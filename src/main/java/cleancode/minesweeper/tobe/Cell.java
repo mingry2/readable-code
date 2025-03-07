e94135e0152b0bd076aa7e52eaccbb4b44e3efd9
minkyung kim <minry20415@naver.com>
2025-03-07 16:36:50 +0900

    섹션4. SOLID

--- src/main/java/cleancode/minesweeper/tobe/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/Cell.java
@@ -7,7 +7,7 @@ public class Cell {
     private static final String UNCHECKED_SIGN = "□";
     private static final String EMPTY_SIGN = "■";
 
-    // final 불편 데이터 명시
+    // final 불변 데이터 명시
     private int nearbyLandMineCount;
     private boolean isLandMine;
     private boolean isFlagged;
@@ -22,8 +22,8 @@ public class Cell {
 
     // 생성자로 new Cell() 말고도 정적 팩터링 메서드로 객체를 생성할 수도 있다.
     // Cell.of(sing); -> 이름을 줄 수 있다. 검증도 서로 다르게 할수도있고
-    public static Cell of(int nearbyLandMineCount, boolean isLandMine, boolean isFlagged, boolean isOccupied) {
-        return new Cell(nearbyLandMineCount, isLandMine, isFlagged, isOccupied);
+    public static Cell of(int nearbyLandMineCount, boolean isLandMine, boolean isFlagged, boolean isOpened) {
+        return new Cell(nearbyLandMineCount, isLandMine, isFlagged, isOpened);
     }
 
     public static Cell create() { // 빈 셀을 생성
@@ -34,8 +34,32 @@ public class Cell {
         this.isLandMine = true;
     }
 
+    public void flag() {
+        this.isFlagged = true;
+    }
+
     public void open() {
         this.isOpened = true;
+    }
+
+    public boolean isChecked() {
+        return isFlagged || isOpened;
+    }
+
+    public boolean isLandMine() {
+        return isLandMine;
+    }
+
+    public boolean isOpened() {
+        return isOpened;
+    }
+
+    public boolean hasLandMineCount() {
+        return this.nearbyLandMineCount != 0;
+    }
+
+    public void updateNearbyLandMineCount(int count) {
+        this.nearbyLandMineCount = count;
     }
 
     public String getSign() {
@@ -56,29 +80,5 @@ public class Cell {
         }
 
         return UNCHECKED_SIGN;
-    }
-
-    public void updateNearbyLandMineCount(int count) {
-        this.nearbyLandMineCount = count;
-    }
-
-    public void flag() {
-        this.isFlagged = true;
-    }
-
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
-    public boolean isLandMine() {
-        return isLandMine;
-    }
-
-    public boolean isOpened() {
-        return isOpened;
-    }
-
-    public boolean hasLandMineCount() {
-        return this.nearbyLandMineCount != 0;
     }
 }
